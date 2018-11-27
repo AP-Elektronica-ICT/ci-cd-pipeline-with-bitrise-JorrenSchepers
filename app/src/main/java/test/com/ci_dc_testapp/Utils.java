2fb0b5c9d41f5d421eed9ad4b7ccb7cb8dcdda58
Jorren schepers <s094476@ap.be>
2018-11-27 11:39:33 +0100

    first commit

---
+++ app/src/main/java/test/com/ci_dc_testapp/Utils.java
@@ -0,0 +1,11 @@
+package test.com.ci_dc_testapp;
+
+/**
+ * Created by Jorren on 27/11/2018.
+ */
+
+public class Utils {
+    public static int addNumbers(int first, int second) {
+        return first + second;
+    }
+}
